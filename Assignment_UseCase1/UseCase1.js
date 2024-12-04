a798da4f715bbe7fc07deb9ef9520c96a816d345
Shruticdh <shruticdh2003@gmail.com>
2024-12-04 10:57:55 +0530

    Shruti Add : UseCase1

---
+++ Assignment_UseCase1/UseCase1.js
@@ -0,0 +1,12 @@
+// use const to make variacble that will not change 
+const isPresent = 1;
+
+// let variable to find random number no. 0 or 1
+let employeeCheck = Math.floor(Math.random() * 2);
+
+if(employeeCheck === isPresent){
+    console.log("Employee Present");
+}
+else{
+    console.log("Employee Not Present");
+}
